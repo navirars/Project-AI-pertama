40448219fe94afd7aebe7ba97ea354cefc924c57
navirars <79823144+navirars@users.noreply.github.com>
2021-10-31 20:41:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import numpy as np
 import cv2
 import glob
 
-img_dir1 = "Dataset/NANGKA/"
-img_dir2 = "Dataset/SIRIH/"
+imdir1 = "Dataset/NANGKA/"
+imdir2 = "Dataset/SIRIH/"
 
 ext = ['jpg']
 
 files1 = []
 files2 = []
 
-[files1.extend(glob.glob(img_dir1 + '*.' + e)) for e in ext]
-[files2.extend(glob.glob(img_dir2 + '*.' + e)) for e in ext]
+[files1.extend(glob.glob(imdir1 + '*.' + e)) for e in ext]
+[files2.extend(glob.glob(imdir2 + '*.' + e)) for e in ext]
 
 images1 = [cv2.imread(file1) for file1 in files1]
 images2 = [cv2.imread(file2) for file2 in files2]
